6fd2bdcaca4345a88cdb40138753ac7ca04e7cda
daemosity <evanescentink@gmail.com>
2024-12-28 14:52:50 -0600

    created pygame group classes drawable and updatable, added player character, now using group classes in game loop

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ def main():
     clock = pygame.time.Clock()
     deltatime = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        player.draw(screen)
-        player.update(deltatime)
+        
+        for thing in updatable:
+            thing.update(deltatime)
+        for thing in drawable:
+            thing.draw(screen)
+        
         pygame.display.flip()
         
         # limit framerate to 60 FPS
