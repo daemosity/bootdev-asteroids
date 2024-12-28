522f53484d1223c70f5118c8e1fbb18dbeca3c13
daemosity <evanescentink@gmail.com>
2024-12-28 14:27:07 -0600

    added circleShape (hitbox class) and player files/classes, added player constant, and player to main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import pygame
 
+from player import Player
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock()
     deltatime = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        # limit framerate to 60 FPS
         deltatime = clock.tick(60) / 1000
         
     
